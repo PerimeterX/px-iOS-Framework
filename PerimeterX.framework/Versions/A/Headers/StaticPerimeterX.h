5850497c59cc86a780f3eb0db3fc3125371937de
Oded Regev <odedre@gmail.com>
2017-07-13 13:34:16 +0300

    update framework to be *static* to fix issues that cocoapods encountered with dynamic framework

---
+++ PerimeterX.framework/Versions/A/Headers/StaticPerimeterX.h
@@ -0,0 +1,12 @@
+//
+//  PerimeterX.h
+//  PerimeterX
+//
+//  Created by Oded Regev on 6/4/17.
+//  Copyright © 2017 PerimeterX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <PerimeterX/PXManager.h>
+#import <PerimeterX/PXBlockResponse.h>
