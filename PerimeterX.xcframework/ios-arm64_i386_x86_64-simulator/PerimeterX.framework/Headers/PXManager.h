3393066d5955c22176bc470124289c1b167c6feb
Oren Yaar <oren.yaar@perimeterx.com>
2021-11-22 16:15:00 +0200

    update SDK binary and version to 1.16.2

--- PerimeterX.xcframework/ios-arm64_i386_x86_64-simulator/PerimeterX.framework/Headers/PXManager.h
+++ PerimeterX.xcframework/ios-arm64_i386_x86_64-simulator/PerimeterX.framework/Headers/PXManager.h
@@ -46,7 +46,6 @@ extern NSString *const PX_AUTH_HEADER_KEY;
 @property (nonatomic, assign) NSString *challengeLocale;
 
 -(void) startWith:(NSString *)appId;
--(void) startWith:(NSString *)appId enableDoctorCheck:(BOOL)enableDoctorCheck;
 
 -(void) setCustomParameters:(NSArray *)customParams __deprecated __deprecated_msg("Please use setCustomParametersDictionary");
 -(void) setCustomParametersDictionary:(NSDictionary *)customParamsDictionary;
