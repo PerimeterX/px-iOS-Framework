dd16a6bd0372baf3a01e4da17a07ee15158f4836
Oren Yaar <oren.yaar@perimeterx.com>
2022-01-06 14:39:11 +0200

    Update SDK to v2.0.0

---
+++ PerimeterX_SDK.xcframework/ios-arm64/PerimeterX_SDK.framework/Headers/PerimeterX.h
@@ -0,0 +1,14 @@
+//
+//  PerimeterX.h
+//  PerimeterX
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PerimeterX.
+FOUNDATION_EXPORT double PerimeterXVersionNumber;
+
+//! Project version string for PerimeterX.
+FOUNDATION_EXPORT const unsigned char PerimeterXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerimeterX/PublicHeader.h>
